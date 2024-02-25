938ddb319955ccb64dc98cc20930cb6bc7f83e6b
adrian456 <adrian.borovnik@student.um.si>
2024-02-25 14:20:42 +0100

    feat: :sparkles: Add print method to Activity struct

--- main.go
+++ main.go
@@ -8,20 +8,41 @@ import (
 	"net/http"
 	"os"
 	"reflect"
+
+	"github.com/jedib0t/go-pretty/v6/table"
 )
 
 type Activity struct {
 	Activity      string  `json:"activity"`
-	Accessibility float32 `json:"accessibility"`
 	Type          string  `json:"type"`
 	Participants  uint8   `json:"participants"`
+	Accessibility float32 `json:"accessibility"`
 	Price         float32 `json:"price"`
 	Link          string  `json:"link"`
-	Key           string  `json:"key"`
+	// Key           string  `json:"key"`
+}
+
+func (a *Activity) print() {
+	values := reflect.ValueOf(*a)
+	types := values.Type()
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+
+	t.AppendRow([]interface{}{types.Field(0).Name, values.Field(0)})
+	t.AppendSeparator()
+
+	for i := 1; i < values.NumField(); i++ {
+		t.AppendRow([]interface{}{types.Field(i).Name, values.Field(i)})
+	}
+
+	t.Render()
 }
 
 func main() {
-	response, err := http.Get("http://www.boredapi.com/api/activity/")
+	const BORED_API_URL string = "http://www.boredapi.com/api/activity/"
+
+	response, err := http.Get(BORED_API_URL)
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
@@ -34,10 +55,5 @@ func main() {
 
 	var activity Activity
 	json.Unmarshal(data, &activity)
-
-	values := reflect.ValueOf(activity)
-	types := values.Type()
-	for i := 0; i < values.NumField(); i++ {
-		fmt.Println(types.Field(i).Name, values.Field(i))
-	}
+	activity.print()
 }
